44cf88f58c8a42fe0e0e53687fc96f782f0a391d
Samuel <user@Users-MacBook-Pro.local>
2019-11-03 08:28:11 +1100

    Initial express server with route files.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://Samuel:19820323@devconnect-shard-00-00-ifaoo.mongodb.net:27017,devconnect-shard-00-01-ifaoo.mongodb.net:27017,devconnect-shard-00-02-ifaoo.mongodb.net:27017/test?ssl=true&replicaSet=Devconnect-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log(err));
+
+
+app.get('/', (req, res) => res.send("Hello there working? yes."));
+
+app.use('/routes/api/users', users);
+app.use('/routes/api/profile', profile);
+app.use('/routes/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`))
+
